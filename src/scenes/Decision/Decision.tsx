747b2a91d0ea3e69a4235b726fbe8745590c77df
ChristianOffice <christian.joecker@maibornwolff.de>
2020-04-05 20:19:35 +0200

    Decision.tsx done - still needs to be tested

--- src/scenes/Decision/Decision.tsx
+++ src/scenes/Decision/Decision.tsx
@@ -1,33 +1,14 @@
-import React, { Component } from "react";
-import OptionsAndCriteria from "./1_OptionsAndCriteria/OptionsAndCriteria";
-import Result from "./4_Result/Result";
-import Typography from "@material-ui/core/Typography";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core";
+import React, {useState } from "react";
 import Step from "@material-ui/core/Step";
 import Stepper from "@material-ui/core/Stepper";
 import StepButton from "@material-ui/core/StepButton";
 import Fab from "@material-ui/core/Fab";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import OptionsRating from "./3_RateOptions/RateOptions";
-import WeightCriteria from "./2_WeightCriteria/WeightCriteria";
-import { connect } from "react-redux";
-import ReactGA from "react-ga";
+import { makeStyles } from "@material-ui/core/styles";
+import theme from "../../muiTheme";
 
-function TabContainer(props) {
-	return (
-		<Typography component="div" style={{ padding: 8 * 3 }}>
-			{props.children}
-		</Typography>
-	);
-}
-
-TabContainer.propTypes = {
-	children: PropTypes.node.isRequired,
-};
-
-const styles = (theme) => ({
+const useStyles = makeStyles({
 	divMain: {
 		flexGrow: 1,
 		width: "100%",
@@ -39,205 +20,148 @@ const styles = (theme) => ({
 		backgroundColor: "transparent",
 	},
 
-	button_next: {
+	buttonNext: {
 		position: "fixed",
 		bottom: 0,
 		right: 0,
 		margin: theme.spacing(1),
 	},
 
-	button_back: {
+	buttonBack: {
 		position: "fixed",
 		bottom: 0,
 		left: 0,
 		margin: theme.spacing(1),
 	},
 });
-function getSteps() {
-	return [
-		"Write options and criteria",
-		"Weight criteria",
-		"Rate options",
-		"Result",
-	];
-}
 
-class Decision extends Component {
-	constructor(props) {
-		super(props);
-
-		this.getStepContent = this.getStepContent.bind(this);
-
-		this.state = {
-			activeStep: 0,
-			stepComplete: {},
-			decisionId: 0,
-		};
-	}
-
-	componentDidMount() {
-		this.setState({ decisionId: this.props.match.params.decisionId });
-	}
-
-	handleNext = () => {
-		if (!isNaN(this.state.activeStep)) {
-			ReactGA.event({
-				category: "Decision",
-				action: "Go To Next Step",
-				value: this.state.activeStep + 1,
-			});
-		}
-
-		//Mark Step as Done
-		this.changeStepDone();
-
-		//Change Step
-		this.setState((state) => ({
-			activeStep: state.activeStep + 1,
-		}));
-	};
-
-	handleBack = () => {
-		if (!isNaN(this.state.activeStep)) {
-			ReactGA.event({
-				category: "Decision",
-				action: "Go To Prev Step",
-				value: this.state.activeStep - 1,
-			});
-		}
-
-		//Mark Step as Done
-		this.changeStepDone();
-
-		//Change Step
-		this.setState((state) => ({
-			activeStep: state.activeStep - 1,
-		}));
-	};
-
-	handleStep = (step) => () => {
-		if (!isNaN(step)) {
-			ReactGA.event({
-				category: "Decision",
-				action: "Jump To Step",
-				value: step,
-			});
-		}
-
-		//Mark Step as Done
-		this.changeStepDone();
-
-		//Change Step
-		this.setState({
-			activeStep: step,
-		});
-	};
-
-	changeStepDone() {
-		let stepComplete = this.state.stepComplete;
-		stepComplete[this.state.activeStep] = true;
-
-		this.setState({
-			stepComplete: stepComplete,
-		});
-	}
-
-	getStepContent(stepIndex) {
-		switch (stepIndex) {
-			case 0:
-				return (
-					<OptionsAndCriteria decisionId={this.props.match.params.decisionId} />
-				);
-			case 1:
-				return (
-					<WeightCriteria decisionId={this.props.match.params.decisionId} />
-				);
-			case 2:
-				return (
-					<OptionsRating decisionId={this.props.match.params.decisionId} />
-				);
-			case 3:
-				return <Result decisionId={this.props.match.params.decisionId} />;
-			default:
-				return "Unknown stepIndex";
-		}
-	}
-
-	render() {
-		const { classes } = this.props;
-		const steps = getSteps();
-		const { activeStep } = this.state;
-
-		const minItemsThere = !(
-			this.props.optionsAndCriteria.decisionOptions.length >= 2 &&
-			this.props.optionsAndCriteria.selectionCriteria.length >= 2
-		);
-
-		const stepDisabled = [false, minItemsThere, minItemsThere, minItemsThere];
-
-		return (
-			<div className={classes.divMain}>
-				<Stepper
-					className={classes.stepper}
-					alternativeLabel
-					nonLinear
-					activeStep={activeStep}
-				>
-					{steps.map((label, index) => {
-						return (
-							<Step key={label}>
-								<StepButton
-									onClick={this.handleStep(index)}
-									completed={this.state.stepComplete[index]}
-									disabled={stepDisabled[index]}
-								>
-									{label}
-								</StepButton>
-							</Step>
-						);
-					})}
-				</Stepper>
-
-				{this.getStepContent(activeStep)}
-
-				{/*Navigation Buttons*/}
-				{activeStep !== 0 ? (
-					<Fab
-						color="secondary"
-						aria-label="Previous Step"
-						size="medium"
-						className={classes.button_back}
-						onClick={this.handleBack}
-					>
-						<ArrowBackIcon />
-					</Fab>
-				) : null}
-
-				{activeStep !== 3 ? (
-					<Fab
-						color="primary"
-						aria-label="Next Step"
-						size="medium"
-						className={classes.button_next}
-						onClick={this.handleNext}
-						disabled={minItemsThere}
-					>
-						<ArrowForwardIcon />
-					</Fab>
-				) : null}
-			</div>
-		);
-	}
-}
-
-Decision.propTypes = {
-	classes: PropTypes.object.isRequired,
-	optionsAndCriteria: PropTypes.object.isRequired,
+type stepsType = {
+	number: number;
+	name: string;
+	disabled: boolean;
+	completed: boolean;
 };
 
-const mapStateToProps = (state) => ({
-	app: state.app,
-	optionsAndCriteria: state.optionsAndCriteria,
-});
+const Decision: React.FC = () => {
+	const [activeStepNum, setActiveStepNum] = useState(1);
+	const [steps, setSteps] = useState<stepsType[]>([
+		{
+			number: 1,
+			name: "Options and criteria",
+			disabled: false,
+			completed: false,
+		},
+		{
+			number: 2,
+			name: "Weight criteria",
+			disabled: false,
+			completed: false,
+		},
+		{
+			number: 3,
+			name: "Rate options",
+			disabled: false,
+			completed: false,
+		},
+		{
+			number: 4,
+			name: "Result",
+			disabled: false,
+			completed: false,
+		},
+	]);
 
-export default connect(mapStateToProps, null)(withStyles(styles)(Decision));
+	const classes = useStyles();
+
+	const setStepCompleted = (stepNumber: number): void => {
+		const newSteps = [...steps];
+		const index = newSteps.findIndex((obj) => obj.number === stepNumber);
+		newSteps[index].completed = true;
+		
+		setSteps(newSteps);
+	};
+
+	const changeStep = (stepNumber: number): void => {
+		setStepCompleted(stepNumber);
+
+		setActiveStepNum(stepNumber);
+	};
+
+	// TODO disable steps
+
+	// getStepContent(stepIndex) {
+	// 	switch (stepIndex) {
+	// 		case 0:
+	// 			return (
+	// 				<OptionsAndCriteria decisionId={this.props.match.params.decisionId} />
+	// 			);
+	// 		case 1:
+	// 			return (
+	// 				<WeightCriteria decisionId={this.props.match.params.decisionId} />
+	// 			);
+	// 		case 2:
+	// 			return (
+	// 				<OptionsRating decisionId={this.props.match.params.decisionId} />
+	// 			);
+	// 		case 3:
+	// 			return <Result decisionId={this.props.match.params.decisionId} />;
+	// 		default:
+	// 			return "Unknown stepIndex";
+	// 	}
+	// }
+
+	return (
+		<div className={classes.divMain}>
+			<Stepper
+				className={classes.stepper}
+				alternativeLabel
+				nonLinear
+				activeStep={activeStepNum}
+			>
+				{steps.map((step) => {
+					return (
+						<Step key={step.number}>
+							<StepButton
+								onClick={(): void => changeStep(step.number)}
+								completed={step.completed}
+								disabled={step.disabled}
+							>
+								{step.name}
+							</StepButton>
+						</Step>
+					);
+				})}
+			</Stepper>
+
+			{activeStepNum}
+
+			{/*Navigation Buttons*/}
+			{activeStepNum !== 0 ? (
+				<Fab
+					color="secondary"
+					aria-label="Previous Step"
+					size="medium"
+					className={classes.buttonBack}
+					onClick={(): void => changeStep(activeStepNum - 1)}
+				>
+					<ArrowBackIcon />
+				</Fab>
+			) : null}
+
+			{activeStepNum !== steps.length - 1 ? (
+				<Fab
+					color="primary"
+					aria-label="Next Step"
+					size="medium"
+					className={classes.buttonNext}
+					onClick={(): void => changeStep(activeStepNum + 1)}
+					// disabled={minItemsThere}
+				>
+					<ArrowForwardIcon />
+				</Fab>
+			) : null}
+		</div>
+	);
+};
+
+export default Decision;
