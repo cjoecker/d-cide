6b2b74ef49d9d7cc902fbdfea2c012d7d4170cd3
ChristianOffice <christian.joecker@maibornwolff.de>
2020-04-14 11:32:51 +0200

    Redraw decisions list
    Move buttons out of papers

--- src/scenes/Decisions/Decisions.tsx
+++ src/scenes/Decisions/Decisions.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {makeStyles} from "@material-ui/core/styles";
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
 import AddIcon from "@material-ui/icons/Add";
@@ -8,11 +8,13 @@ import DeleteIcon from "@material-ui/icons/DeleteOutlineRounded";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import {shallowEqual, useDispatch, useSelector} from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import {
 	getDecisions,
 	postDecision,
-	changeDecision, deleteDecision, newDecision,
+	changeDecision,
+	deleteDecision,
+	newDecision,
 } from "../../services/redux/actionsAndSlicers/DecisionsActions";
 import Fab from "@material-ui/core/Fab";
 import Grid from "@material-ui/core/Grid";
@@ -20,13 +22,14 @@ import Typography from "@material-ui/core/Typography";
 import TwoButtonsDialog from "../../components/TwoButtonsDialog";
 import IconButton from "@material-ui/core/IconButton/IconButton";
 import theme from "../../muiTheme";
-import {RootState} from "../../services/redux/rootReducer";
-import {useHistory} from "react-router-dom";
-import {Decision} from "../../services/redux/actionsAndSlicers/DecisionsSlice";
+import { RootState } from "../../services/redux/rootReducer";
+import { useHistory } from "react-router-dom";
+import { Decision } from "../../services/redux/actionsAndSlicers/DecisionsSlice";
+import { deleteOptionsAndCriteria } from "../../services/redux/actionsAndSlicers/OptionsAndCriteriaActions";
 
 const useStyles = makeStyles({
 	divMain: {
-		paddingTop: theme.spacing(8),
+		paddingTop: theme.spacing(6),
 		textAlign: "center",
 	},
 
@@ -34,17 +37,30 @@ const useStyles = makeStyles({
 		marginTop: theme.spacing(1),
 	},
 
-	gridItem: {
+	gridItemPaper: {
 		maxWidth: theme.spacing(63),
 	},
 
-	paperNewDecision: {
-		marginTop: theme.spacing(0.8),
-		marginBottom: theme.spacing(4),
+	decisionsNegativeMargin: {
+		marginTop:-theme.spacing(1),
 	},
 
-	paperDecisions: {
-		marginTop: theme.spacing(0.8),
+	gridButtons: {
+		maxWidth: theme.spacing(7),
+		marginLeft: theme.spacing(1.5),
+	},
+
+	paperNewDecision: {
+		marginTop: theme.spacing(1),
+		marginBottom: theme.spacing(1),
+	},
+
+	gridItemDecisions: {
+		marginTop: theme.spacing(2),
+	},
+
+	paperButtons: {
+		marginRight: -theme.spacing(1.5),
 	},
 
 	newEntry: {
@@ -59,11 +75,13 @@ const useStyles = makeStyles({
 });
 
 const Decisions: React.FC = () => {
-
 	const [componentLoaded, setComponentLoaded] = useState(false);
 	const [newEntry, setNewEntry] = useState("");
 	const [showAskBeforeDelete, setShowAskBeforeDelete] = useState(false);
-	const [decisionToBeDeleted, setDecisionToBeDeleted] = useState<Decision>({id: 0, name:""});
+	const [decisionToBeDeleted, setDecisionToBeDeleted] = useState<Decision>({
+		id: 0,
+		name: "",
+	});
 	const [localDecisions, setLocalDecisions] = useState<Decision[]>([]);
 
 	const decisions = useSelector(
@@ -71,7 +89,7 @@ const Decisions: React.FC = () => {
 		shallowEqual
 	);
 
-	const { user} = useSelector(
+	const { user } = useSelector(
 		(state: RootState) => state.Session,
 		shallowEqual
 	);
@@ -82,30 +100,30 @@ const Decisions: React.FC = () => {
 
 	useEffect(() => {
 		getDecisions(dispatch);
-		setComponentLoaded(true)
+		setComponentLoaded(true);
 	}, []);
 
 	useEffect(() => {
-		if(componentLoaded && !user.registeredUser) history.push(`/decisions/${decisions[0].id}`);
+		if (componentLoaded && !user.registeredUser)
+			history.push(`/decisions/${decisions[0].id}`);
 		setLocalDecisions(decisions);
 	}, [decisions]);
 
 	//TODO go to the decision, when new decision is created
-		// if (
-		// 	prevProps.decision.decisions.length !== 0 &&
-		// 	prevProps.decision.decisions.length <
-		// 		this.props.decision.decisions.length &&
-		// 	this.state.isMounted === true
-		// ) {
-		// 	const prevSet = new Set(prevProps.decision.decisions.map((o) => o.id));
-		// 	const added = this.props.decision.decisions.filter(
-		// 		(o) => !prevSet.has(o.id)
-		// 	);
-		//
-		// 	let decisionId = added[0].id;
-		// 	this.goToDecision(decisionId);
-		// }
-
+	// if (
+	// 	prevProps.decision.decisions.length !== 0 &&
+	// 	prevProps.decision.decisions.length <
+	// 		this.props.decision.decisions.length &&
+	// 	this.state.isMounted === true
+	// ) {
+	// 	const prevSet = new Set(prevProps.decision.decisions.map((o) => o.id));
+	// 	const added = this.props.decision.decisions.filter(
+	// 		(o) => !prevSet.has(o.id)
+	// 	);
+	//
+	// 	let decisionId = added[0].id;
+	// 	this.goToDecision(decisionId);
+	// }
 
 	const createDecision = () => {
 		//Exit if entry
@@ -118,94 +136,91 @@ const Decisions: React.FC = () => {
 		postDecision(dispatch, newDecision);
 
 		setNewEntry("");
-	}
+	};
 
 	const onChangeDecision = (event, decisionId: number) => {
 		setLocalDecisions(
 			localDecisions.map((decision) =>
-				decision.id === decisionId ? { ...decision, name: event.target.value } : decision
+				decision.id === decisionId
+					? { ...decision, name: event.target.value }
+					: decision
 			)
 		);
 	};
 
 	const onLeaveDecision = (decision: Decision) => {
-		if (decision.name !== "")
-			changeDecision(dispatch, decision);
-		else
-			showDeleteDialog(decision)
+		if (decision.name !== "") changeDecision(dispatch, decision);
+		else showDeleteDialog(decision);
 	};
 
 	const onDeleteDecision = () => {
-		deleteDecision(dispatch, decisionToBeDeleted)
+		deleteDecision(dispatch, decisionToBeDeleted);
 		setShowAskBeforeDelete(false);
 	};
 
-
-	const showDeleteDialog = (decision: Decision)  => {
+	const showDeleteDialog = (decision: Decision) => {
 		setShowAskBeforeDelete(true);
 		setDecisionToBeDeleted(decision);
-	}
+	};
 
-		return (
-				<div className={classes.divMain}>
-					<Typography
-						variant="h3"
-						className={classes.title}
-						gutterBottom
+	return (
+		<div className={classes.divMain}>
+			<Typography variant="h3" className={classes.title} gutterBottom>
+				Decisions
+			</Typography>
+			<Grid container justify="center" alignItems="center">
+				<Grid item xs={11} className={classes.gridItemPaper}>
+					<List>
+						<Paper
+							elevation={2}
+							key="New Entry"
+							className={classes.paperNewDecision}
+						>
+							<ListItem>
+								<InputBase
+									name="newEntry"
+									className={classes.newEntry}
+									placeholder="New Decision"
+									value={newEntry}
+									onKeyPress={(event) => {
+										if (event.key === "Enter") {
+											event.preventDefault();
+											createDecision();
+										}
+									}}
+									onChange={(event) => setNewEntry(event.target.value)}
+									multiline
+								/>
+							</ListItem>
+						</Paper>
+					</List>
+				</Grid>
+				<Grid item xs={1} className={classes.gridButtons}>
+					<Fab
+						size="small"
+						color="primary"
+						aria-label="Add"
+						onClick={createDecision}
 					>
-						Decisions
-					</Typography>
-					<Grid container justify="center">
-						<Grid item xs={12} className={classes.gridItem}>
-							<List>
-								<Paper
-									elevation={2}
-									key="New Entry"
-									className={classes.paperNewDecision}
-								>
-									<ListItem>
-										<InputBase
-											name="newEntry"
-											className={classes.newEntry}
-											placeholder="New Decision"
-											value={newEntry}
-											onKeyPress={(event) => {
-												if (event.key === "Enter") {
-													event.preventDefault();
-													createDecision();
-												}
-											}}
-											onChange={(event) => setNewEntry(event.target.value)}
-											multiline
-										/>
-										<ListItemSecondaryAction>
-											{newEntry.length > 0 ? (
-												<Fab
-													size="small"
-													color="primary"
-													aria-label="Add"
-													onClick={createDecision}
-												>
-													<AddIcon />
-												</Fab>
-											) : null}
-										</ListItemSecondaryAction>
-									</ListItem>
-								</Paper>
-								{decisions.map((decision) => (
+						<AddIcon />
+					</Fab>
+				</Grid>
+
+				<Grid item xs={12} className={classes.gridItemDecisions}>
+					{decisions.map((decision) => (
+						<Grid container justify="center" alignItems="center">
+							<Grid item xs={11} className={`${classes.gridItemPaper} ${classes.decisionsNegativeMargin}`}>
+								<List>
 									<Paper
 										elevation={2}
 										key={decision.id}
-										className={classes.paperDecisions}
 									>
 										<ListItem>
 											<InputBase
 												multiline
 												className={classes.inputBaseExistingItems}
 												value={decision.name}
-												onChange={(event) =>
-													onChangeDecision(event, decision)
-												}
+												onChange={(event) => onChangeDecision(event, decision)}
 												onBlur={() => onLeaveDecision(decision)}
 												onKeyPress={(event) => {
 													if (event.key === "Enter") {
@@ -218,43 +233,47 @@ const Decisions: React.FC = () => {
 											/>
 											<ListItemSecondaryAction>
 												<IconButton
-													size="small"
-													color="secondary"
 													aria-label="Delete"
-													onClick={() =>
-														showDeleteDialog(decision)
-													}
-													style={{ marginRight: 7 }}
+													onClick={() => showDeleteDialog(decision)}
+													className={classes.paperButtons}
 												>
 													<DeleteIcon />
 												</IconButton>
-												<Fab
-													size="small"
-													color="primary"
-													aria-label="Work on Decision"
-													onClick={() => history.push(`/decisions/${decision.id}`)}
-												>
-													<ArrowForwardIcon />
-												</Fab>
 											</ListItemSecondaryAction>
 										</ListItem>
 									</Paper>
-								))}
-							</List>
+								</List>
+							</Grid>
+							<Grid
+								item
+								xs={1}
+								justify="center"
+								className={`${classes.gridButtons} ${classes.decisionsNegativeMargin}`}
+							>
+								<Fab
+									size="small"
+									color="primary"
+									aria-label="Work on Decision"
+									onClick={() => history.push(`/decisions/${decision.id}`)}
+								>
+									<ArrowForwardIcon />
+								</Fab>
+							</Grid>
 						</Grid>
-					</Grid>
+					))}
+				</Grid>
+			</Grid>
+			<TwoButtonsDialog
+				show={showAskBeforeDelete}
+				title={`Delete ${decisionToBeDeleted.name}?`}
+				message="Your decision will be permanently deleted. This cannot be undone."
+				primaryButtonText="Delete it"
+				secondaryButtonText="Cancel"
+				onClickPrimary={onDeleteDecision}
+				onClickSecondary={() => setShowAskBeforeDelete(false)}
+			/>
+		</div>
+	);
+};
 
-					<TwoButtonsDialog
-						show={showAskBeforeDelete}
-						title={`Delete ${decisionToBeDeleted.name}?`}
-						message="Your decision will be permanently deleted. This cannot be undone."
-						primaryButtonText="Delete it"
-						secondaryButtonText="Cancel"
-						onClickPrimary={onDeleteDecision}
-						onClickSecondary={() => setShowAskBeforeDelete(false)}
-					/>
-				</div>
-		);
-}
-
-export default Decisions
+export default Decisions;
