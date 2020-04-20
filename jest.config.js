c348f89086e764a880658ac4a4716e2cfe59e29e
ChristianOffice <christian.joecker@maibornwolff.de>
2020-04-20 19:03:49 +0200

    Add EditableList.test.tsx but is still not working

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		"<rootDir>/src"
 	],
 	"transform": {
-		"^.+\\.ttsx?$": "ts-jest",
+		"^.+\\.tsx?$": "ts-jest",
 	},
 	"testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 	"moduleFileExtensions": [
--- src/scenes/Decision/Step1OptionsAndCriteria/components/EditableList.test.tsx
+++ src/scenes/Decision/Step1OptionsAndCriteria/components/EditableList.test.tsx
@@ -1,53 +1,54 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createMount } from "@material-ui/core/test-utils";
 import { InputBase } from "@material-ui/core";
 import store from "../../../../services/redux/store";
 import EditableList from "./EditableList";
 import { OptionsAndCriteriaKeys } from "../../../../services/redux/actionsAndSlicers/OptionsAndCriteriaSlice";
 import { NOT_ENOUGH_OPTIONS } from "../../../../services/Alerts";
+import { MemoryRouter } from "react-router-dom";
+import { createMount } from "@material-ui/core/test-utils";
 
-let mount:any;
-let wrapper:any;
+let mount: any;
+let wrapper: any;
 
 beforeEach(() => {
 	mount = createMount();
 
 	wrapper = mount(
-		<Provider store={store}>
-			<EditableList
-				itemsKey={OptionsAndCriteriaKeys.decisionOptions}
-				notEnoughItemsAlert={NOT_ENOUGH_OPTIONS}
-				hidden={false}
-			/>
-		</Provider>
+		<MemoryRouter initialEntries={['/decisions/1']}>
+				<Provider store={store}>
+					<EditableList
+						itemsKey={OptionsAndCriteriaKeys.decisionOptions}
+						notEnoughItemsAlert={NOT_ENOUGH_OPTIONS}
+						hidden={false}
+					/>
+				</Provider>
+		</MemoryRouter>
 	);
 });
 
-afterEach(() => {
-	mount.cleanUp();
-});
-
-
-
-const findComponent = (value:string) => {
+const findComponent = (value: string) => {
 	return wrapper.find(`[data-testid='${value}']`).first();
 };
 
+it('allows user to write input for new item', () => {
+	const inputBase = findComponent('input-base');
 
-it("allows user to write input for new item", () => {
-	const inputBase = findComponent("input-base");
-	const value = "New item";
+	const value = 'New item';
+
 
 	wrapper
 		.find(InputBase)
-		.at(0)
+		.childAt(0)
 		.props()
-		.onChange({ target: { name: "InputBase", value: value } });
+		.onChange({
+				target: {name: 'InputBase', value},
+			});
 
-	inputBase.simulate("change", {
-		target: { name: "InputBase", value: value },
-	});
+	// inputBase.simulate('change', {
+	// 	target: {name: 'InputBase', value},
+	// });
+	console.log(inputBase.prop("value"));
 
-	expect(inputBase.prop("value")).toBe(value);
+	expect(inputBase.prop('value')).toBe(value);
 });
