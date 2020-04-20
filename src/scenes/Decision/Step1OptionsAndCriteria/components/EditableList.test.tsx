69d616bc91aad77ec8b891fb3937c2ee94339f6b
ChristianOffice <christian.joecker@maibornwolff.de>
2020-04-20 20:02:59 +0200

    Drop enzyme for testing
    It is not necessary anymore. All the functions working with react testing library

--- src/scenes/Decision/Step1OptionsAndCriteria/components/EditableList.test.tsx
+++ src/scenes/Decision/Step1OptionsAndCriteria/components/EditableList.test.tsx
@@ -11,6 +11,8 @@ import "@testing-library/jest-dom/extend-expect";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let getByTestId: any;
 
+
+
 beforeEach(() => {
 	getByTestId = render(
 		<MemoryRouter initialEntries={['/decisions/1']}>
