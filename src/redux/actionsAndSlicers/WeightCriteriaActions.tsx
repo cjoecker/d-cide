a26e603cfb0f95ef8768dee6d3b89d09410a7346
ChristianOffice <christian.joecker@maibornwolff.de>
2020-04-26 11:39:46 +0200

    Refactor WeightCriteriaActions

--- src/redux/actionsAndSlicers/WeightCriteriaActions.tsx
+++ src/redux/actionsAndSlicers/WeightCriteriaActions.tsx
@@ -26,11 +26,12 @@ export const updateWeightedCriteria = (dispatch: AppDispatch, decisionId: string
 };
 
 const updateErrorAction: ErrorActionType = (dispatch, error, predefinedPayload: WeightedCriteria) => {
-
 	showHTTPAlert(dispatch, error);
+	resetSliderOnError(dispatch, predefinedPayload)
+};
 
+const resetSliderOnError = (dispatch: AppDispatch, predefinedPayload: WeightedCriteria) => {
 	const state = store.getState();
 	const initialCriteria = state.WeightedCriteria.find(criteria => criteria.id === predefinedPayload.id)
-
 	dispatch(WeightedCriteriaSlice.actions.updateWeightedCriteria(initialCriteria as WeightedCriteria))
 };
