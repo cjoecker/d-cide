618b81dd6f648ca9f85019f27b9a41c2c5045e15
ChristianOffice <christian.joecker@maibornwolff.de>
2020-04-29 11:00:20 +0200

    Refactor Step4Results.spec

--- cypress/integration/Step4Results.spec.ts
+++ cypress/integration/Step4Results.spec.ts
@@ -16,30 +16,7 @@ context('Actions', () => {
 		});
 
 		it('shows diagram correctly', () => {
-			cy
-				.getTestElement('decisionOptionsDiagram')
-				.children()
-				.should('contain', '0')
-				.and('contain', '2.5')
-				.and('contain', '5')
-				.and('contain', '7.5')
-				.and('contain', '10')
-
-				.and('contain', 'House 1')
-				.and('contain', 'House 2')
-				.and('contain', 'House 3')
-
-				.get('[name="House 3"]')
-				.should('have.length', 1)
-				.get('[name="House 2"]')
-				.should('have.length', 1)
-				.get('[name="House 1"]')
-				.should('have.length', 1);
-
-			cy.getTestElement('decisionOptionsDiagram').within(() => {
-				cy.get('.recharts-cartesian-grid-vertical').should('have.length', 1);
-				cy.get('.recharts-layer.recharts-yAxis').should('have.length', 1);
-			});
+			showsDiagramCorrectly('decisionOptions', ["House 1","House 2","House 3"])
 		});
 
 		it('wraps long labels', () => {
@@ -57,33 +34,7 @@ context('Actions', () => {
 		});
 
 		it('shows diagram correctly', () => {
-			cy
-				.getTestElement('selectionCriteriaDiagram')
-				.children()
-				.should('contain', '0')
-				.and('contain', '2.5')
-				.and('contain', '5')
-				.and('contain', '7.5')
-				.and('contain', '10')
-
-				.and('contain', 'Kitchen')
-				.and('contain', 'Size')
-				.and('contain', 'Neighborhood')
-				.and('contain', 'Garden')
-
-				.get('[name="Kitchen"]')
-				.should('have.length', 1)
-				.get('[name="Size"]')
-				.should('have.length', 1)
-				.get('[name="Neighborhood"]')
-				.should('have.length', 1)
-		.get('[name="Garden"]')
-				.should('have.length', 1);
-
-			cy.getTestElement('selectionCriteriaDiagram').within(() => {
-				cy.get('.recharts-cartesian-grid-vertical').should('have.length', 1);
-				cy.get('.recharts-layer.recharts-yAxis').should('have.length', 1);
-			});
+			showsDiagramCorrectly('selectionCriteria', ["Kitchen","Size","Neighborhood","Garden"])
 		});
 
 		it('wraps long labels', () => {
@@ -120,5 +71,31 @@ context('Actions', () => {
 			.getTestElement(`${sectionName}Diagram`)
 			.contains(/1234567890123-(\n)*4567890/);
 	};
-	
+
+	const showsDiagramCorrectly = (sectionName: string, items: string[]) => {
+		cy
+			.getTestElement(`${sectionName}Diagram`)
+			.children()
+			.should('contain', '0')
+			.and('contain', '2.5')
+			.and('contain', '5')
+			.and('contain', '7.5')
+			.and('contain', '10');
+
+		items.map(item => {
+			cy.getTestElement(`${sectionName}Diagram`).children().should('contain', item);
+		});
+
+		cy.getTestElement(`${sectionName}Diagram`).within(() => {
+			cy
+				.get('.recharts-cartesian-grid-vertical')
+				.should('have.length', 1)
+				.get('.recharts-layer.recharts-yAxis')
+				.should('have.length', 1)
+				.get('g[class="recharts-layer recharts-bar-rectangle"]')
+				.should('have.length', items.length)
+				.get('text[class="recharts-text recharts-label"]')
+				.should('have.length', items.length);
+		});
+	};
 });
