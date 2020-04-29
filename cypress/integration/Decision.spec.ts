620b947e7e63fbea3ea2bd594bfc1e7adccb3dd0
ChristianOffice <christian.joecker@maibornwolff.de>
2020-04-29 15:13:16 +0200

    Add step completed test

--- cypress/integration/Decision.spec.ts
+++ cypress/integration/Decision.spec.ts
@@ -12,11 +12,16 @@ context('Actions', () => {
 			.click()
 			.root()
 			.should('contain', 'Weight Criteria')
+			.get('.MuiStepIcon-completed')
+			.should('have.length', 1)
 
 			.getTestElement('NextStepButton')
 			.click()
 			.root()
 			.should('contain', 'Rate Options')
+			.get('.MuiStepIcon-completed')
+			.should('have.length', 2)
+
 
 			.getTestElement('NextStepButton')
 			.click()
@@ -24,11 +29,15 @@ context('Actions', () => {
 			.should('contain', 'Decision Options Ranking')
 			.and('contain', 'Selection Criteria Ranking')
 			.getTestElement('NextStepButton').should("have.length", 0)
+			.get('.MuiStepIcon-completed')
+			.should('have.length', 3)
 
 			.getTestElement('PrevStepButton')
 			.click()
 			.root()
 			.should('contain', 'Rate Options')
+			.get('.MuiStepIcon-completed')
+			.should('have.length', 4)
 
 			.getTestElement('PrevStepButton')
 			.click()
