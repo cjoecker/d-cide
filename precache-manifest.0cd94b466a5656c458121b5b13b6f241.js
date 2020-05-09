309f844c750aa7fd7c84739b27f9d3cae955e103
ChristianOffice <christian.joecker@maibornwolff.de>
2020-05-09 14:46:34 +0200

    Updates

---
+++ precache-manifest.0cd94b466a5656c458121b5b13b6f241.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8245693277313f7abc5ccc48e23db1",
+    "url": "/d-cide/index.html"
+  },
+  {
+    "revision": "4ce79809ab24b94cd58e",
+    "url": "/d-cide/static/css/main.09a4d6f4.chunk.css"
+  },
+  {
+    "revision": "1b57a6f85c88184ae597",
+    "url": "/d-cide/static/js/2.8d348663.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/d-cide/static/js/2.8d348663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce79809ab24b94cd58e",
+    "url": "/d-cide/static/js/main.1066a83e.chunk.js"
+  },
+  {
+    "revision": "f10dd2b0a5e0f9fd0df5",
+    "url": "/d-cide/static/js/runtime-main.31f3953a.js"
+  },
+  {
+    "revision": "58d8bf4dfc4fa3aafe805c77c482bf21",
+    "url": "/d-cide/static/media/MaibornWolff_Logo.58d8bf4d.svg"
+  },
+  {
+    "revision": "9d73cbb7eaf21fbf1d891a288ad04a6d",
+    "url": "/d-cide/static/media/d-cide_Logo.9d73cbb7.svg"
+  }
+]);
