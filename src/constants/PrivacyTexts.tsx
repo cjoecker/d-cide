b4d03230b7567ac0dcb7886ead65412460d6ed05
ChristianOffice <christian.joecker@maibornwolff.de>
2020-05-15 15:47:33 +0200

    Reword cookie consent

--- src/constants/PrivacyTexts.tsx
+++ src/constants/PrivacyTexts.tsx
@@ -12,7 +12,7 @@ export const PrivacyText = (
 			Cookie Consent
 		</h2>
 		<p>
-			<b>We don't save your data!</b> It stays inside of your computer and belongs only to you. We only use cookies to
+			<b>We don't save your data!</b> It stays inside of your device and belongs only to you. We only use cookies to
 			analyze our traffic and user experience. To be sure of what we do, you can check the full code of this app in the{' '}
 			<Link href='https://github.com/cjoecker/d-cide' underline='always'>
 				GitHub repository
