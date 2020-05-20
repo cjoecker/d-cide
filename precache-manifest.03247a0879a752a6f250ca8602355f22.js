c220d515da1f6fb50452103bbf7cc240d2143670
ChristianOffice <christian.joecker@maibornwolff.de>
2020-05-20 21:53:28 +0200

    Updates

--- precache-manifest.03247a0879a752a6f250ca8602355f22.js
+++ precache-manifest.03247a0879a752a6f250ca8602355f22.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d870853073dc059dc584bd096b0db03c",
+    "revision": "0b72c8fd366fffd51405d9b4c2a99727",
     "url": "/index.html"
   },
   {
-    "revision": "88b7ec03cac43c1dbfd6",
+    "revision": "a8379e806a8fa3364c7a",
     "url": "/static/css/main.103801d0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b82d8874.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "88b7ec03cac43c1dbfd6",
-    "url": "/static/js/main.b87e37f8.chunk.js"
+    "revision": "a8379e806a8fa3364c7a",
+    "url": "/static/js/main.68cd151a.chunk.js"
   },
   {
     "revision": "a34a311849e7fa88f520",
