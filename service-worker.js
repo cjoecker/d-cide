2e62e3d3e479f7a4725611fed9a65aded4cb1b44
cjoecker <46030630+cjoecker@users.noreply.github.com>
2020-05-21 13:09:34 +0000

    Deploying to gh-pages from  @ 3a5704562b3a781d35dc2df0d6db04f12b03e21b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03247a0879a752a6f250ca8602355f22.js"
+  "/precache-manifest.661256b96ab2539fe644e914704537c0.js"
 );
 
 self.addEventListener('message', (event) => {
