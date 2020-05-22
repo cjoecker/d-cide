7d713aceb31761efa4b3da149dac7dfe1e8d4cc5
ChristianOffice <christian.joecker@maibornwolff.de>
2020-05-22 22:13:00 +0200

    Add google analytics again

--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -21,8 +21,6 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
 	});
 }
 
-console.log('hola');
-
 export type AppDispatch = typeof store.dispatch;
 
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
