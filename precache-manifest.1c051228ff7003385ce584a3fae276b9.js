a00f644845637a04b920107db97f39212f6596f4
cjoecker <46030630+cjoecker@users.noreply.github.com>
2020-05-23 21:33:06 +0000

    Deploying to gh-pages from  @ d33ccc9025d4db2a1094dc8af075a10bf1b37257 🚀

---
+++ precache-manifest.1c051228ff7003385ce584a3fae276b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b23e25726217b5c3e2787aed86f4e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e0760def25429286178",
+    "url": "/static/css/main.43f68bc9.chunk.css"
+  },
+  {
+    "revision": "84a487700574bc34da00",
+    "url": "/static/js/2.d4b646fd.chunk.js"
+  },
+  {
+    "revision": "175a010fb93cc5ce15469da651301399",
+    "url": "/static/js/2.d4b646fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0760def25429286178",
+    "url": "/static/js/main.4d6c3296.chunk.js"
+  },
+  {
+    "revision": "a34a311849e7fa88f520",
+    "url": "/static/js/runtime-main.5939600b.js"
+  },
+  {
+    "revision": "d2461716d83178369db41767e4ab0426",
+    "url": "/static/media/d-cide_Logo.d2461716.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1784e9900ab197136315faba7af4cf05.js"
+  "/precache-manifest.1c051228ff7003385ce584a3fae276b9.js"
 );
 
 self.addEventListener('message', (event) => {
