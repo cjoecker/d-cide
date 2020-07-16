5bd3d489c82b55f6dd90c76ff15f9220bb1237b6
Christian Jöcker <kontakt@cjoecker.de>
2020-07-16 16:55:39 +0200

    Add framer-motion to instructions

--- src/components/InstructionsBox.tsx
+++ src/components/InstructionsBox.tsx
@@ -5,6 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Grid from '@material-ui/core/Grid';
 import {shallowEqual, useSelector} from 'react-redux';
+import {AnimatePresence, motion} from 'framer-motion';
 import {RootState} from '../services/redux/rootReducer';
 import {instructions} from '../constants/Instructions';
 
@@ -26,9 +27,9 @@ const useStyles = makeStyles<Theme, StyleProps>(theme => ({
 		height: '100%',
 		borderRadius: theme.spacing(1),
 		boxShadow: '0px 0px 41px -11px rgba(0,0,0,0.7)',
-		animation: `$myEffect 2000ms`,
-		animationIterationCount: 'infinite',
-		animationTimingFunction: 'easeInOutSine',
+		// animation: `$myEffect 2000ms`,
+		// animationIterationCount: 'infinite',
+		// animationTimingFunction: 'easeInOutSine',
 	},
 	instructionsBoxLeft: {
 		'&::after': {
@@ -57,6 +58,17 @@ const useStyles = makeStyles<Theme, StyleProps>(theme => ({
 			transform: 'translateY(0)',
 		},
 	},
+	'@keyframes popOut': {
+		'0%': {
+			transform: 'scale(0)',
+		},
+		'80%': {
+			transform: 'scale(1.2)',
+		},
+		'100%': {
+			transform: 'scale(1)',
+		},
+	},
 	text: {
 		color: 'black',
 	},
@@ -81,6 +93,33 @@ const useStyles = makeStyles<Theme, StyleProps>(theme => ({
 	},
 }));
 
+const startAnimation = {
+	visible: {
+		scale: 1,
+		transition: {
+			type: 'spring',
+			delay: 0.2,
+			stiffness: 200,
+		},
+	},
+	hidden: {scale: 0, duration: 0.2},
+};
+
+const loopAnimation = {
+	to: {
+		translateY: -5,
+		transition: {
+			delay: 1.5,
+			duration: 0.3,
+			yoyo: Infinity,
+			when: 'afterChildren',
+		},
+	},
+	from: {translateY: 0},
+};
+
+//TODO check if after animation button can be tabbed
+
 type ComponentsTooltipProps = {
 	show: boolean;
 };
@@ -95,41 +134,53 @@ const InstructionsBox = (props: ComponentsTooltipProps) => {
 		setIsVisible(show);
 	}, [show]);
 
+	useEffect(() => {
+		setIsVisible(show);
+	}, [instructionsSteps]);
+
 	const classes = useStyles(instructions[instructionsSteps]);
 
 	return (
-		<>
+		<AnimatePresence exitBeforeEnter>
 			{isVisible && (
-				<div className={`${classes.instructionsBox} ${classes.instructionsBoxLeft}`}>
-					<Grid container justify='flex-start' alignContent='flex-start'>
-						<Grid item xs={10}>
-							<div className={classes.typography}>
-								<Typography component='span' variant='body1' align='left' color={'secondary'}>
-									{instructions[instructionsSteps].text}
-								</Typography>
-							</div>
+				<motion.div variants={loopAnimation} initial={'from'} animate={'to'}>
+					<motion.div
+						className={`${classes.instructionsBox} ${classes.instructionsBoxLeft}`}
+						variants={startAnimation}
+						initial={'hidden'}
+						animate={'visible'}
+						exit={'hidden'}
+					>
+						<Grid container justify='flex-start' alignContent='flex-start'>
+							<Grid item xs={10}>
+								<div className={classes.typography}>
+									<Typography component='span' variant='body1' align='left' color='secondary'>
+										{instructions[instructionsSteps].text}
+									</Typography>
+								</div>
+							</Grid>
+							<Grid item xs={2}>
+								<IconButton aria-label='delete' className={classes.closeButton} onClick={() => setIsVisible(false)}>
+									<CloseIcon fontSize='small' />
+								</IconButton>
+							</Grid>
+							<Grid item justify='flex-start' alignContent='flex-start' xs={12}>
+								<Box top={0} left={0} position='relative' display='flex' alignItems='left' justifyContent='left'>
+									<Button
+										className={classes.linkButton}
+										data-testid='dontShowMoreHelp'
+										onClick={() => setIsVisible(false)}
+										color='primary'
+									>
+										Don&apos;t show help anymore
+									</Button>
+								</Box>
+							</Grid>
 						</Grid>
-						<Grid item xs={2}>
-							<IconButton aria-label='delete' className={classes.closeButton} onClick={() => setIsVisible(false)}>
-								<CloseIcon fontSize='small' />
-							</IconButton>
-						</Grid>
-						<Grid item justify='flex-start' alignContent='flex-start' xs={12}>
-							<Box top={0} left={0} position='relative' display='flex' alignItems='left' justifyContent='left'>
-								<Button
-									className={classes.linkButton}
-									data-testid='dontShowMoreHelp'
-									onClick={() => setIsVisible(false)}
-									color='primary'
-								>
-									Don&apos;t show help anymore
-								</Button>
-							</Box>
-						</Grid>
-					</Grid>
-				</div>
+					</motion.div>
+				</motion.div>
 			)}
-		</>
+		</AnimatePresence>
 	);
 };
 
