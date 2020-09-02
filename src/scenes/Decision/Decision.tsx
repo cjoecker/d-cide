20d18d1fcabba35032493bc96921f125d597fc2c
Christian Jöcker <kontakt@cjoecker.de>
2020-09-02 09:27:50 +0200

    fix swippeableView animation problem

--- src/scenes/Decision/Decision.tsx
+++ src/scenes/Decision/Decision.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Step from '@material-ui/core/Step';
 import Stepper from '@material-ui/core/Stepper';
 import StepButton from '@material-ui/core/StepButton';
@@ -13,7 +13,7 @@ import ReactGA from 'react-ga';
 import {isEdge} from 'react-device-detect';
 
 import Grid from '@material-ui/core/Grid';
-import {motion} from 'framer-motion';
+import {AnimatePresence, motion} from 'framer-motion';
 import OptionsAndCriteria from './Step1OptionsAndCriteria/OptionsAndCriteria';
 import WeightCriteria from './Step2WeightCriteria/WeightCriteria';
 import RateOptions from './Step3RateOptions/RateOptions';
@@ -25,12 +25,24 @@ import InstructionsBox from '../../components/InstructionsBox';
 
 const useStyles = makeStyles(theme => ({
   divMain: {
-    flexGrow: 1,
     width: '100%',
     overflowX: 'hidden',
   },
+  divStepsContainer: {
+    display: 'inline-flex',
+  },
+  divSteps: {
+    width: '100vw',
+  },
   stepper: {
     backgroundColor: 'transparent',
+  },
+  gridButtons: {
+    position: 'fixed',
+    zIndex: 1000,
+    width: '100%',
+    bottom: 0,
+    padding: theme.spacing(0, 2, 2, 0),
   },
   stepperLabel: {
     marginBottom: theme.spacing(-1.5),
@@ -43,13 +55,6 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(0, 0, 2, 2),
     zIndex: 1000,
   },
-  divParent: {
-    position: 'fixed',
-    zIndex: 1000,
-    width: '100%',
-    bottom: 0,
-    padding: theme.spacing(0, 2, 2, 0),
-  },
 }));
 
 type stepsType = {
@@ -57,6 +62,27 @@ type stepsType = {
   name: string;
   disabled: boolean;
   completed: boolean;
+};
+
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
 };
 
 const Decision: React.FC = () => {
@@ -90,10 +116,14 @@ const Decision: React.FC = () => {
     },
   ]);
 
+  const [[page, direction], setPage] = useState([0, 0]);
+
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
   const {alerts} = useSelector((state: RootState) => state.App, shallowEqual);
   const {instructionsSteps} = useSelector((state: RootState) => state.App, shallowEqual);
-
-  const nextButtonRef = useRef(null);
 
   const classes = useStyles();
   const theme = useTheme();
@@ -133,18 +163,17 @@ const Decision: React.FC = () => {
     );
   };
 
-  const setStepCompleted = (stepNumber: number) => {
-    const newSteps = [...steps];
-    const index = newSteps.findIndex(obj => obj.number === stepNumber);
-    newSteps[index].completed = true;
-
-    setSteps(newSteps);
-  };
+  // const setStepCompleted = (stepNumber: number) => {
+  //   const newSteps = [...steps];
+  //   const index = newSteps.findIndex(obj => obj.number === stepNumber);
+  //   newSteps[index].completed = true;
+  //
+  //   setSteps(newSteps);
+  // };
 
   const changeStep = (stepNumber: number, element: string) => {
-    setStepCompleted(activeStepNum);
-    console.log(stepNumber);
-    setActiveStepNum(stepNumber);
+    // setStepCompleted(activeStepNum);
+    // setActiveStepNum(stepNumber);
     window.scrollTo(0, 0);
 
     ReactGA.event({
@@ -152,106 +181,125 @@ const Decision: React.FC = () => {
       action: `Change to step ${stepNumber} with ${element}`,
     });
   };
+
+  useEffect(() => {
+    console.log(page);
+  }, [page]);
+
+  const stepsComponents = [
+    <OptionsAndCriteria hidden={false} />,
+    <WeightCriteria hidden={false} />,
+    <RateOptions hidden={false} />,
+    <Results hidden={false} />,
+  ];
+
   //TODO remove tabIndex={-1} when this ticket is solved:
   //	https://github.com/mui-org/material-ui/issues/21208
   return (
-    <>
-      <div className={classes.divMain}>
-        <Stepper className={classes.stepper} alternativeLabel nonLinear activeStep={activeStepNum - 1}>
-          {steps.map(step => {
-            return (
-              <Step key={step.number}>
-                <ComponentsTooltip>
-                  <StepButton
-                    focusRipple
-                    data-testid={`Step${step.number}Button`}
-                    onClick={() => changeStep(step.number, 'step button')}
-                    completed={step.completed}
-                    disabled={step.disabled}
-                    aria-label={`Go to step ${step.number}`}
-                  >
-                    <StepLabel
-                      StepIconProps={{
-                        classes: {
-                          root: classes.stepperLabel,
-                        },
-                      }}
-                    >
-                      {step.name}
-                    </StepLabel>
-                  </StepButton>
-                </ComponentsTooltip>
-              </Step>
-            );
-          })}
-        </Stepper>
-
-        <Grid container className={classes.divParent} justify='flex-end' alignItems='flex-end' wrap='nowrap'>
-          <Grid item>
-            {activeStepNum !== 1 && (
+    <div className={classes.divMain}>
+      <Stepper className={classes.stepper} alternativeLabel nonLinear activeStep={activeStepNum - 1}>
+        {steps.map(step => {
+          return (
+            <Step key={step.number}>
               <ComponentsTooltip>
-                <Fab
-                  data-testid='PrevStepButton'
-                  color='secondary'
-                  aria-label='Previous step'
-                  size='medium'
-                  className={classes.buttonBack}
-                  style={{
-                    bottom: isEdge ? 10 : 'env(safe-area-inset-bottom)',
-                    left: isEdge ? 10 : 'env(safe-area-inset-left)',
-                  }}
-                  onClick={() => changeStep(activeStepNum - 1, 'previous button')}
+                <StepButton
+                  focusRipple
+                  data-testid={`Step${step.number}Button`}
+                  onClick={() => changeStep(step.number, 'step button')}
+                  completed={step.completed}
+                  disabled={step.disabled}
+                  aria-label={`Go to step ${step.number}`}
                 >
-                  <ArrowBackIcon />
-                </Fab>
+                  <StepLabel StepIconProps={{classes: {root: classes.stepperLabel}}}>{step.name}</StepLabel>
+                </StepButton>
               </ComponentsTooltip>
-            )}
-          </Grid>
-          <Grid style={{width: '100%'}} item>
-            <InstructionsBox show={instructionsSteps === 5} />
-          </Grid>
-          <Grid item>
-            {activeStepNum !== steps.length && (
-              <ComponentsTooltip>
-                <Fab
-                  data-testid='NextStepButton'
-                  color='primary'
-                  aria-label='Next step'
-                  size='medium'
-                  className={classes.buttonNext}
-                  onClick={() => changeStep(activeStepNum + 1, 'next button')}
-                  disabled={disableStepButtons}
-                  style={{
-                    marginBottom: isEdge ? 10 : 'env(safe-area-inset-bottom',
-                    marginRight: isEdge ? 10 : 'env(safe-area-inset-right)',
-                  }}
-                >
-                  <ArrowForwardIcon />
-                </Fab>
-              </ComponentsTooltip>
-            )}
-          </Grid>
-        </Grid>
-
-        <SwipeableViews
-          disabled
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={activeStepNum - 1}
-          onTransitionEnd={() => {
-            console.log(activeStepNum);
-            setLoadedStepNum(activeStepNum);
-          }}
-          style={{
-            zIndex: 0,
-          }}
-        >
-          <OptionsAndCriteria hidden={loadedStepNum !== 1} />
-          <WeightCriteria hidden={loadedStepNum !== 2} />
-          <RateOptions hidden={loadedStepNum !== 3} />
-          <Results hidden={loadedStepNum !== 4} />
-        </SwipeableViews>
+            </Step>
+          );
+        })}
+      </Stepper>
+      <div className={classes.divStepsContainer}>
+        <AnimatePresence initial={false} custom={direction} exitBeforeEnter>
+          <motion.div
+            className={classes.divSteps}
+            key={page}
+            custom={direction}
+            variants={variants}
+            initial='enter'
+            animate='center'
+            exit='exit'
+            transition={{
+              x: {type: 'spring', stiffness: 300, damping: 200},
+              opacity: {duration: 0.2},
+            }}
+          >
+            {stepsComponents[page]}
+          </motion.div>
+        </AnimatePresence>
       </div>
-    </>
+      <Grid container className={classes.gridButtons} justify='flex-end' alignItems='flex-end' wrap='nowrap'>
+        <Grid item>
+          {page !== 0 && (
+            <ComponentsTooltip>
+              <Fab
+                data-testid='PrevStepButton'
+                color='secondary'
+                aria-label='Previous step'
+                size='medium'
+                className={classes.buttonBack}
+                style={{
+                  bottom: isEdge ? 10 : 'env(safe-area-inset-bottom)',
+                  left: isEdge ? 10 : 'env(safe-area-inset-left)',
+                }}
+                onClick={() => paginate(-1)}
+                //onClick={() => changeStep(activeStepNum - 1, 'previous button')}
+              >
+                <ArrowBackIcon />
+              </Fab>
+            </ComponentsTooltip>
+          )}
+        </Grid>
+        <Grid style={{width: '100%'}} item>
+          <InstructionsBox show={instructionsSteps === 5} />
+        </Grid>
+        <Grid item>
+          {page !== steps.length && (
+            <ComponentsTooltip>
+              <Fab
+                data-testid='NextStepButton'
+                color='primary'
+                aria-label='Next step'
+                size='medium'
+                className={classes.buttonNext}
+                onClick={() => paginate(1)}
+                // onClick={() => changeStep(activeStepNum + 1, 'next button')}
+                disabled={disableStepButtons}
+                style={{
+                  marginBottom: isEdge ? 10 : 'env(safe-area-inset-bottom',
+                  marginRight: isEdge ? 10 : 'env(safe-area-inset-right)',
+                }}
+              >
+                <ArrowForwardIcon />
+              </Fab>
+            </ComponentsTooltip>
+          )}
+        </Grid>
+      </Grid>
+
+      {/*<SwipeableViews*/}
+      {/* disabled*/}
+      {/* axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}*/}
+      {/* index={activeStepNum - 1}*/}
+      {/* onTransitionEnd={() => {*/}
+      {/*   setLoadedStepNum(activeStepNum);*/}
+      {/* }}*/}
+      {/* style={{zIndex: 0}}*/}
+      {/*>*/}
+      {/*  <OptionsAndCriteria hidden={loadedStepNum !== 1} />*/}
+      {/*  <WeightCriteria hidden={loadedStepNum !== 2} />*/}
+      {/*  <RateOptions hidden={loadedStepNum !== 3} />*/}
+      {/*  <Results hidden={loadedStepNum !== 4} />*/}
+      {/*</SwipeableViews>*/}
+    </div>
   );
 };
 
