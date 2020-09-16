282d8c10ecfc6aba4d942a9d7cc0b8459cdb0f6c
Christian Jöcker <kontakt@cjoecker.de>
2020-09-16 14:13:04 +0200

    add word wrap unit tests

---
+++ src/services/wrapWord.test.ts
@@ -0,0 +1,12 @@
+import wrapWord from './wrapWord';
+
+it('wrap word', () => {
+  expect(wrapWord('12345', 3)).toEqual('123-\n45');
+});
+it("doesn't wrap short words", () => {
+  expect(wrapWord('12345', 5)).toEqual('12345');
+});
+
+it("doesn't wrap words with spaces", () => {
+  expect(wrapWord('1 2345', 3)).toEqual('1 2345');
+});
